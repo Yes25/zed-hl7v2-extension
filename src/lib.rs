a1d233953c107b5d0d92c892aed04f74b7cd0604
jesse <jesse.kruse@posteo.de>
2025-05-29 23:54:46 +0200

    invoke lsp

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
+use crate::zed::LanguageServerId;
 use zed_extension_api as zed;
+use zed_extension_api::Result;
 
 struct Hl7V2;
 
 impl zed::Extension for Hl7V2 {
     fn new() -> Self {
         Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        Ok(zed::Command {
+            command: String::from(
+                "/Users/jessekruse/other_Code/rustProjects/hl7_v2_lsp/target/debug/hl7_v2_lsp",
+            ),
+            args: Vec::new(),
+            env: Vec::new(),
+        })
     }
 }
 
